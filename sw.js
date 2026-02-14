af16154bfbdebfca2fb4c8c187531055f3d1bfce
旅途中看海 <554605951@qq.com>
2026-02-14 13:25:51 +0800

    first commit

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'gps-altimeter-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+  // 如果有本地图标文件，也加在这里，例如 './icon-192.png'
+];
+
+// 安装 SW
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// 激活并清理旧缓存
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    })
+  );
+});
+
+// 拦截请求
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
